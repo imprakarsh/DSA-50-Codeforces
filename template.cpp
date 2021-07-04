d1fe220108855f5467a5dde060ee4bc2925e3d21
imprakarsh <prakarsh1213@gmail.com>
2021-07-04 12:34:19 +0530

    initial template

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define int long long 
+using namespace std;
+void solve() { 
+}
+int32_t main(){
+	iostream::sync_with_stdio(false); 
+	cin.tie(0); 
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	#endif
+	int tc = 1; 
+    //~ cin >> tc; 
+	while (tc--) 
+	solve(); 			
+	return 0; 
+	
+}
